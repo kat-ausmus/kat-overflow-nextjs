7094ea38fae21cac5fa08d0c72a562cf9708d4a3
kat-ausmus <katausmus@gmail.com>
2025-11-07 15:32:52 -0600

    WIP: Initial NextJS App

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div>
+            <h1 className="text-3xl"> Welcome to the the Ultimate Next JS course</h1>
+        </div>
+    )
+}
+export default Page
