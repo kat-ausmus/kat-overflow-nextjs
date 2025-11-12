f1bdb5e100583bf7fe34553453206df57e401a49
kat-ausmus <katausmus@gmail.com>
2025-11-12 11:44:51 -0600

    WIP: Theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 import localFont from "next/font/local";
+import {ThemeProvider} from "next-themes";
 
 const inter = localFont({
     src: './fonts/InterVR.ttf',
@@ -13,6 +14,9 @@ const inter = localFont({
 export const metadata: Metadata = {
   title: "Kat Overflow App",
   description: "Stackoverflow like app built with Next.js",
+    icons: {
+      icon: "/favicon.ico",
+    }
 };
 
 export default function RootLayout({
@@ -21,11 +25,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            >
+            {children}
+        </ThemeProvider>
       </body>
     </html>
   );
