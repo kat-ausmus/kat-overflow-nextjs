756af777b1f10b1ed01466e7ec705c438479e3ac
kat-ausmus <katausmus@gmail.com>
2025-11-12 14:08:31 -0600

    WIP: Add ShadCN DropDown component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Page = () => {
+
+const Home = () => {
     return (
         <div className="card">
         Welcome to the the Ultimate Next JS course &#128075;
         </div>
     )
 }
-export default Page
+export default Home
