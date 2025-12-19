ea85a98290b306103aee4754a7fa901d75c220ca
kat-ausmus <katausmus@gmail.com>
2025-12-19 16:53:49 -0600

    WIP: set a default avatar icon if author image DNE

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -34,7 +34,7 @@ const QuestionCard = ({ question: { _id, title, tags, author, createdAt, upvotes
 
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
         <Metric
-          imgUrl={author.image}
+          imgUrl={author.image || "/icons/avatar.svg"}
           alt={author.name}
           value={author.name}
           title={`• asked ${getTimeStamp(createdAt)}`}
