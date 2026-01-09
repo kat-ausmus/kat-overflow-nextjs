42c503db71e864a403f03c5e341dce83ca66c66f
kat-ausmus <katausmus@gmail.com>
2026-01-09 16:32:01 -0600

    WIP: added collection model

--- database/collection.model.ts
+++ database/collection.model.ts
@@ -1,8 +1,17 @@
-import { model, models, Schema } from 'mongoose';
+import { model, models, Schema, Types } from 'mongoose';
 
-export interface ICollection {}
+export interface ICollection {
+  author: Types.ObjectId;
+  question: Types.ObjectId;
+}
 
-const CollectionSchema = new Schema<ICollection>({}, { timestamps: true });
+const CollectionSchema = new Schema<ICollection>(
+  {
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
+    question: { type: Schema.Types.ObjectId, ref: 'Question' },
+  },
+  { timestamps: true }
+);
 
 const Collection = models?.Collection || model<ICollection>('Collection', CollectionSchema);
 
