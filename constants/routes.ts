082d7c112579fabc5208054404eed6d0c01a53ed
kat-ausmus <katausmus@gmail.com>
2026-01-16 11:57:35 -0600

    WIP: implemented client api calls

--- constants/routes.ts
+++ constants/routes.ts
@@ -1,11 +1,12 @@
 const ROUTES = {
-  HOME: "/",
-  SIGN_IN: "/sign-in",
-  SIGN_UP: "/sign-up",
-  ASK_QUESTION: "/ask-a-question",
+  HOME: '/',
+  SIGN_IN: '/sign-in',
+  SIGN_UP: '/sign-up',
+  ASK_QUESTION: '/ask-a-question',
   PROFILE: (id: string) => `/profile/${id}`,
   QUESTION: (id: string) => `/question/${id}`,
   TAGS: (id: string) => `/tags/${id}`,
+  SIGN_IN_WITH_OAUTH: `signin-with-oauth`,
 };
 
 export default ROUTES;
--- lib/validations/auth.schema.ts
+++ lib/validations/auth.schema.ts
@@ -44,3 +44,16 @@ export const SignUpSchema = z.object({
     }),
 });
 export type SignUpSchemaType = z.infer<typeof SignUpSchema>;
+
+export const SignInWithOAuthSchema = z.object({
+  provider: z.enum(['google', 'github']),
+  providerAccountId: z.string().min(1, { message: 'Provider Account ID is required.' }),
+  user: z.object({
+    name: z.string().min(1, { message: 'Name is required.' }),
+    username: z.string().min(3, { message: 'Username must be at least 3 characters long.' }),
+    email: z.string().email({ message: 'Please provide a valid email address.' }),
+    image: z.string().url('Invalid image URL').optional(),
+  }),
+});
+
+export type SignInWithOAuthSchemaType = z.infer<typeof SignInWithOAuthSchema>;
