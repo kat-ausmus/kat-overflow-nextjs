4032ce2650852e3c61588d29120b93fbdff8abb2
kat-ausmus <katausmus@gmail.com>
2026-02-04 16:49:04 -0600

    WIP: Updated the mobile navigation and added user avatar.

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,32 +1,35 @@
-import React from 'react'
-import Link from "next/link";
-import Image from "next/image";
-import Theme from "./Theme";
-import MobileNavigation from "@/components/navigation/navbar/MobileNavigation";
+import Link from 'next/link';
+import Image from 'next/image';
+import Theme from './Theme';
+import MobileNavigation from '@/components/navigation/navbar/MobileNavigation';
+import { auth } from '@/auth';
+import UserAvatar from '@/components/UserAvatar';
 
-const NavBar = () => {
-    return (
-        <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
-            <Link href="/" className="flex items-center gap-1">
-                <Image
-                    src="/images/site-logo.svg"
-                    width={23}
-                    height={23}
-                    alt="DevFlow Logo"
-                />
+const NavBar = async () => {
+  const session = await auth();
 
-                <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
-                    Dev<span className="text-primary-500">Flow</span>
-                </p>
-            </Link>
+  return (
+    <nav className="flex-between background-light900_dark200 shadow-light-300 fixed z-50 w-full gap-5 p-6 sm:px-12 dark:shadow-none">
+      <Link href="/" className="flex items-center gap-1">
+        <Image src="/images/site-logo.svg" width={23} height={23} alt="DevFlow Logo" />
 
-            <p>Global Search</p>
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev<span className="text-primary-500">Flow</span>
+        </p>
+      </Link>
 
-            <div className="flex-between gap-5">
-                <Theme />
-                <MobileNavigation />
-            </div>
-        </nav>
-    );
+      <p>Global Search</p>
+
+      <div className="flex-between gap-5">
+        <Theme />
+
+        {session?.user?.id && (
+          <UserAvatar id={session.user.id} name={session.user.name!} imageUrl={session.user?.image} />
+        )}
+
+        <MobileNavigation />
+      </div>
+    </nav>
+  );
 };
 export default NavBar;
